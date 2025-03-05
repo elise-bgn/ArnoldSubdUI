5bfc0fde1d0858d2348c5bad66e3a0fb3518a221
Elise-Bourgoignie <elise.s.bgn@gmail.com>
2025-03-05 09:42:29 +0100

    Made one class for the code to run. Made the first half of the UI

--- main.py
+++ main.py
@@ -1,11 +1,63 @@
 from maya import cmds
 
-selection = cmds.ls(sl=1)
+class ArnoldSubdUI:
+    '''
+    A Maya tool for adjusting the Attribute Editor Arnold Subdivision tab for multiple objects simultaneously
+    '''
 
-# create a loop over each object in selection
+    def __init__(self):
+        #Initialize variables
+        self.window_title = "Set Arnold Subdivision"
+        self.tool_window = self.window_title.replace(" ", "_").casefold()
+        self.selection = cmds.ls(sl=1)
 
-for geo in selection:
-    if cmds.listRelatives(geo)[0] in cmds.ls(et='mesh'):
+        #Build UI
+        self.window_cleaner()
+        self.build_ui()
 
-        cmds.setAttr(geo + ".aiSubdivType", 1);
-        cmds.setAttr(geo + ".aiSubdivIterations", 2);
+    def build_ui(self):
+        """
+        Constructs the user interface for the Batch Renderer tool.
+        """
+        self.tool_window = cmds.window(self.tool_window,
+                                       title=self.window_title,
+                                       widthHeight=(400, 300))
+        cmds.columnLayout(adjustableColumn=True, rowSpacing=10)
+
+        self.mainLayout = cmds.columnLayout(w=250, h=120)
+
+        #Title text
+        cmds.text(label="Arnold Subdivisions",
+                  w=250, h=50, fn="boldLabelFont",
+                  bgc=(0.15,0.15,0.15))
+        cmds.separator(h=10, style="none")
+
+        self.rowLayout = cmds.rowColumnLayout( nc=3, cw= [(1,150),(2,50),(3,50)])
+
+        #Dropdown layout
+        self.subOptionMenu = cmds.optionMenuGrp("subOptionMenu", label="Sub Type", cal=[1, "left"], cw=(1, 47))
+        cmds.menuItem(label="none")
+
+        cmds.showWindow(self.tool_window)
+
+    def window_cleaner(self):
+        """
+        Cleans up any existing UI window with the same name before creating a new one.
+        """
+        if cmds.window(self.tool_window, exists=True):
+            cmds.deleteUI(self.tool_window)
+
+    def set_subdiv_and_iterations(self):
+        '''
+        Loops over the selection and only applies changes on meshes (no cameras, lights,...)
+        '''
+
+        # Create a loop over each object in selection
+
+        for geo in self.selection:
+            if cmds.listRelatives(geo)[0] in cmds.ls(et='mesh'): #only use meshes
+                cmds.setAttr(geo + ".aiSubdivType", 1);
+                cmds.setAttr(geo + ".aiSubdivIterations", 2);
+
+# Run the tool
+ArnoldSubdUI()
